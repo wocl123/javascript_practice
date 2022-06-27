7f8f2e93c3062e5f0c253a502ec65319d7174900
wocl123 <wocl123@naver.com>
2022-06-27 22:37:30 +0900

    토이프로젝트 (네이버 영화 api를 이용한 검색)

---
+++ react_movieapp/src/App.js
@@ -0,0 +1,17 @@
+import "antd/dist/antd.min.css";
+import SearchContainer from "./container/SearchContainer";
+
+function App() {
+  return (
+    <div>
+      <SearchContainer />
+    </div>
+  );
+}
+
+export default App;
+
+/*
+design : https://ant.design/
+=> npm i antd
+*/
